cdb9016a5f88b83c4726b1ff4f47e79bd6370405
Giovanni Intini <intinig@gmail.com>
2007-10-25 07:19:58 +0000

    added rails app
    git-svn-id: file:///srv/svn/myousica/myousica/trunk@2 1758279c-6b0d-4dd2-8212-40bfc25400ca

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '02cedf3882e78b5a99c0bec5cc75c3fc'
+end
