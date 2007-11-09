b51d380ffd2bd639c19ea358e1b7e2db9d4f5f80
Giovanni Intini <intinig@gmail.com>
2007-11-09 11:27:24 +0000

    Aggiunta la homepage e spezzettata come si deve in parziali. Pronto alla stesura dei modelli.
    git-svn-id: file:///srv/svn/myousica/myousica/trunk@9 1758279c-6b0d-4dd2-8212-40bfc25400ca

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def stars(n, color = 'green')
+    result = ""
+    n.times { result << "<img src=\"/images/star_#{color}.png\" alt=\"\" width=\"10\" height=\"9\" />"}
+    result
+  end
+  
 end
