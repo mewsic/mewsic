aa49dd3d9b2ea5f459e555cda09341278c078685
Giovanni Intini <intinig@gmail.com>
2007-11-22 12:29:32 +0000

    implementata la classe per scrollare
    git-svn-id: file:///srv/svn/myousica/myousica/trunk@18 1758279c-6b0d-4dd2-8212-40bfc25400ca

---
+++ app/helpers/multitrack_helper.rb
@@ -0,0 +1,2 @@
+module MultitrackHelper
+end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,14 +1,56 @@
-Event.observe(window, 'load', function(event) {
+var PictureSlider = Class.create({	
+		initialize: function(id) {				
+			this.options = Object.extend({}, arguments[1] || {});				
+			this.working = false;	 
+			this.left_trigger = $(id).down('.trigger.left')
+			this.right_trigger = $(id).down('.trigger.right')
+			this.scrolling_div = $(id).down('.horizontal-scroll div')
+
+			this.setup();
+		},
+
+		setup: function() {
+			Event.observe(this.left_trigger, 'click', this.slideLeft.bind(this));
+			Event.observe(this.right_trigger, 'click', this.slideRight.bind(this));
+		},
+
+		slideLeft: function(event) {
+			this.slide(315);
+			Event.stop(event);
+		},
+
+		slideRight: function(event) {
+			this.slide(-315);
+			Event.stop(event);
+		},
+
+		slide: function(pixel) {
+			if(!this.working) {
+				this.working = true;
+				new Effect.Move(this.scrolling_div, {
+					x: pixel,
+					afterFinish: function(){ this.working = false; }.bind(this),
+					duration: 0.5
+				});
+			}
+		}
+});
+	
+document.observe('dom:loaded', function(event) {
 
 	// Login Behavior
 	if ( $('log-in') != null ) {
-		$('log-in').down('input', 1).focus()
+		$('log-in').down('input', 1).focus();
 	} else {
-		$('search').down('input').focus()
+		$('search').down('input').focus();
 	}
-	
+
 	if ( $('log-in-errors') != null && $('log-in-errors').visible() ) {
-		$('log-in').down('input', 2).clear().focus()
+		$('log-in').down('input', 2).clear().focus();
 	}
 	
+	if ( $('most-friends-scroller') != null ) {
+		new PictureSlider('most-friends-scroller');
+	} 
+
 });
