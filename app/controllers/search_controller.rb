506657ecbe7852c32eea92dfeb399fd941b380bd
Giovanni Intini <intinig@gmail.com>
2008-03-26 16:01:33 +0000

    cambiato il numero di song e track per pagina nel search_controller
    git-svn-id: file:///srv/svn/myousica/myousica/trunk@141 1758279c-6b0d-4dd2-8212-40bfc25400ca

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,8 +3,8 @@ class SearchController < ApplicationController
   def show
     #FIXME: i risultati sono random
     @show_siblings_count = true
-    @songs  = Song.paginate(:page => params[:song_page], :per_page => 10)
-    @tracks = Track.paginate(:page => params[:track_page], :per_page => 10)
+    @songs  = Song.paginate(:page => params[:song_page], :per_page => 4)
+    @tracks = Track.paginate(:page => params[:track_page], :per_page => 4)
     respond_to do |format|
       format.xml
     end
