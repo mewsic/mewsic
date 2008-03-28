11f7e877d3846a1153a1bf7da9e7241191d32327
Andrea Franz <franz.andrea@gmail.com>
2008-03-28 09:34:01 +0000

    nelle liste band vengono visualizzate al max 3 song x user
    git-svn-id: file:///srv/svn/myousica/myousica/trunk@152 1758279c-6b0d-4dd2-8212-40bfc25400ca

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,7 @@ class DashboardController < ApplicationController
     @people = User.find :random, :limit => 6, :include => [:avatars, :songs], :conditions => ["activated_at IS NOT NULL"]
     @answers = Answer.find :all, :order => 'answers.created_at DESC', :limit => 2, :include => [:replies, {:user => :avatars}], :conditions => ["users.activated_at IS NOT NULL"]
     # TODO: Qui dovrei trovare degli utenti e non delle canzoni, ma così evitiamo di avere utenti con canzoni vuote
-    @songs = Song.find :random, :limit => 6, :include => [{:user => [:avatars, :songs]}]
+    @songs = Song.find :random, :limit => 2, :include => [{:user => [:avatars, :songs]}]
   end
   
   def mylist
