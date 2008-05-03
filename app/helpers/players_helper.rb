1e587667c445bee33d74956f004a3b3ffc3b5dba
Andrea Franz <franz.andrea@gmail.com>
2008-05-03 14:55:24 +0000

    changed player dimensions
    git-svn-id: file:///srv/svn/myousica/myousica/trunk@199 1758279c-6b0d-4dd2-8212-40bfc25400ca

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -3,7 +3,7 @@ module PlayersHelper
   def play_button(playable, options = {})    
     link_to(image_tag('button_play_green.png'), send("formatted_#{playable.class.name.downcase}_player_path", playable, 'html'), 
       :class => 'lightwindow',
-      :params => "lightwindow_width=300,lightwindow_height=50,lightwindow_show_images=2"
+      :params => "lightwindow_width=290,lightwindow_height=25,lightwindow_loading_animation=false"
     ) 
   end
   
