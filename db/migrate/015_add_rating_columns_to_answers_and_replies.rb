fac938fc8f53e62d07385d3080a5b6398b2416dc
Andrea Franz <franz.andrea@gmail.com>
2008-05-12 12:35:34 +0000

    git-svn-id: file:///srv/svn/myousica/myousica/trunk@235 1758279c-6b0d-4dd2-8212-40bfc25400ca

---
+++ db/migrate/015_add_rating_columns_to_answers_and_replies.rb
@@ -0,0 +1,11 @@
+class AddRatingColumnsToAnswersAndReplies < ActiveRecord::Migration
+  def self.up
+    Answer.add_ratings_columns
+    Reply.add_ratings_columns
+  end
+
+  def self.down
+    Answer.remove_ratings_columns
+    Reply.remove_ratings_columns
+  end
+end
