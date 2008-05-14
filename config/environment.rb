015bd1dc0049c8a78323b22101f83a24a1f87f4a
Marcello Barnaba <vjt@openssl.it>
2008-05-14 00:14:35 +0000

    added <br> to the tags whitelist
    git-svn-id: file:///srv/svn/myousica/myousica/trunk@259 1758279c-6b0d-4dd2-8212-40bfc25400ca

--- config/environment.rb
+++ config/environment.rb
@@ -59,7 +59,7 @@ end
 
 # Allow few html tags to sanitize()
 ActionView::Base.sanitized_allowed_tags.clear
-ActionView::Base.sanitized_allowed_tags = *%w(strong em b i code small address ul li abbr)
+ActionView::Base.sanitized_allowed_tags = *%w(strong em b i code small address ul li abbr br)
 
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance| 
   "<span class=\"fieldWithErrors\">#{html_tag}</span><span class=\"error\"><img src=\"/images/alert_ico.gif\" alt="" /> #{[instance.error_message].flatten.first}</span>"
