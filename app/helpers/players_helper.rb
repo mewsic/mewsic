f3daca45fc656f231448bcc8d7363894e88049aa
Andrea Franz <franz.andrea@gmail.com>
2008-05-16 19:42:51 +0000

    changed play button color for track and song
    git-svn-id: file:///srv/svn/myousica/myousica/trunk@353 1758279c-6b0d-4dd2-8212-40bfc25400ca

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,7 +1,9 @@
 module PlayersHelper
   
   def play_button(playable, options = {})    
-    options = { :color => :green }.merge(options)
+    options = {}
+    options[:color] = playable.is_a?(Track) ? :orange : :green
+    options.merge(options)
     link_to(image_tag("button_play_#{options[:color]}.png"), send("formatted_#{playable.class.name.downcase}_player_path", playable, 'html'), 
       :class => 'lightwindow',
       :params => "lightwindow_width=290,lightwindow_height=25,lightwindow_loading_animation=false"
