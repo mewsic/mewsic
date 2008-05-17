fb815dcbfbc44552666e7ed0298954d5c5e6767d
Marcello Barnaba <vjt@openssl.it>
2008-05-17 20:00:08 +0000

    restored activation e-mail
    git-svn-id: file:///srv/svn/myousica/myousica/trunk@382 1758279c-6b0d-4dd2-8212-40bfc25400ca

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -4,7 +4,8 @@ class UserObserver < ActiveRecord::Observer
   end
 
   def after_save(user)  
-    UserMailer.deliver_forgot_password(user)  if user.recently_forgot_password?
-    UserMailer.deliver_reset_password(user)   if user.recently_reset_password?
+    UserMailer.deliver_activation(user)      if user.pending?
+    UserMailer.deliver_forgot_password(user) if user.recently_forgot_password?
+    UserMailer.deliver_reset_password(user)  if user.recently_reset_password?
   end
 end
