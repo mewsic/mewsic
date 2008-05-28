510e9fd77c7fd07e9f8fd2b9fbf4af0b659a9846
Marcello Barnaba <vjt@openssl.it>
2008-05-28 23:31:35 +0000

    added MSN messenger on windows integration, added skype callto:// links, added appear/fade effect to the popup.
    git-svn-id: file:///srv/svn/myousica/myousica/trunk@525 1758279c-6b0d-4dd2-8212-40bfc25400ca

--- public/javascripts/user/im.js
+++ public/javascripts/user/im.js
@@ -12,8 +12,7 @@ var IMBox = Class.create({
 
 	handleOpenPopup: function(event) {
 		event.stop();
-		this.popup.show();
-		this.container.show();
+		new Effect.Appear(this.popup, {duration: 0.3});
 
 		element = event.element();
 		if (element.tagName == 'IMG')
@@ -24,8 +23,9 @@ var IMBox = Class.create({
 
 	handleClosePopup: function(event) {
 		event.stop();
-		this.popup.hide();
-		this.container.update('').hide();
+		new Effect.Fade(this.popup, {
+			duration: 0.3,
+			afterFinish: function() { this.container.update(''); }.bind(this) });
 	},
 
 	loadPage: function(url) {
@@ -43,10 +43,49 @@ var IMBox = Class.create({
 	},
 
 	handlePageLoaded: function() {
+		MSNProxy.instance.setupLinks();
 		this.spinner.hide();
+	}
+});
+
+var MSNProxy = Class.create({
+  initialize: function() {
+		this.app = new Element('object', {height:0,width:0});
+
+		try {
+			this.app.classid = "clsid:B69003B3-C55E-4B48-836C-BC5946FC3B28";
+			this.status = this.app.MyStatus;
+			this.msn_present = this.status != null;
+		}
+		catch (e) {
+			this.msn_present = false;
+		}
+	},
+
+	setupLinks: function() {
+		$$('a.msn-link').invoke('observe', 'click', this.onMSNClick.bind(this));
+	},
+
+	onMSNClick: function(event) {
+		event.stop();
+
+		if (!this.msn_present)
+			return;
+
+		if (this.app.MyStatus == 1) {
+			alert("You have to log in to MSN for this to work!");
+			return;
+		}
+
+		var element = event.element();
+		if (element.tagName == 'IMG')
+			element = element.up('a.msn-link');
+
+		this.app.addContact(0, element.getAttribute('rel'));
 	}
 });
 
 document.observe('dom:loaded', function() {
 	IMBox.instance = new IMBox();
+	MSNProxy.instance = new MSNProxy();
 });
