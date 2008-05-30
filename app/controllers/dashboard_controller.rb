88c4ef4f2a898aa4a92e3c98bb63832cfb6fc50b
Marcello Barnaba <vjt@openssl.it>
2008-05-30 05:22:18 +0000

    show only published songs in the dashboard
    git-svn-id: file:///srv/svn/myousica/myousica/trunk@573 1758279c-6b0d-4dd2-8212-40bfc25400ca

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,7 @@ class DashboardController < ApplicationController
     @people = User.find :random, :limit => 6, :include => [:avatars, :songs], :conditions => ["activated_at IS NOT NULL"]
     @answers = Answer.find :all, :order => 'answers.created_at DESC', :limit => 2, :include => [{:user => :avatars}], :conditions => ["users.activated_at IS NOT NULL"]
     # TODO: Qui dovrei trovare degli utenti e non delle canzoni, ma così evitiamo di avere utenti con canzoni vuote
-    @songs = Song.find :random, :limit => 3, :include => [{:user => [:avatars, :songs]}]
+    @songs = Song.find :random, :limit => 3, :include => [{:user => [:avatars, :songs]}], :conditions => ['published = ?', true]
     @ideas = Track.find_orphans(:limit => 2)
   end
   
