5bc37cb7d0cf0541663a0b63f6da795758d4314b
Marcello Barnaba <vjt@openssl.it>
2008-07-07 18:00:21 +0000

    myousica: admin: added track/song creation, user management, destroy support
    git-svn-id: file:///srv/svn/myousica/myousica/trunk@831 1758279c-6b0d-4dd2-8212-40bfc25400ca

--- app/controllers/admin/songs_controller.rb
+++ app/controllers/admin/songs_controller.rb
@@ -2,14 +2,36 @@ class Admin::SongsController < Admin::AdminController
   def index
     @songs = Song.find(:all, :conditions => ['published = ?', true], :order => 'id')
   end
+
+  def new
+    @song = Song.new
+    render :action => 'show'
+  end
   
   def show
     @song = Song.find(params[:id])
   end
 
+  def create
+    @song = Song.create!(params[:song])
+    @track = Track.new :song_id => @song.id
+    render :template => 'admin/tracks/show'
+
+  rescue ActiveRecord::ActiveRecordError
+    render :action => 'show'
+  end
+
   def update
     @song = Song.find(params[:id])
     @song.update_attributes! params[:song]
+    render(:update) { |page| page.hide 'editing' }
+
+  rescue ActiveRecord::ActiveRecordError
     render :action => 'show'
+  end
+
+  def destroy
+    Song.find(params[:id]).destroy
+    render(:update) { |page| page.hide 'editing' }
   end
 end
--- app/controllers/admin/tracks_controller.rb
+++ app/controllers/admin/tracks_controller.rb
@@ -3,13 +3,39 @@ class Admin::TracksController < Admin::AdminController
     @tracks = Track.find(:all, :order => 'title', :order => 'id')
   end
 
+  def new
+    @track = Track.new
+    render :action => 'show'
+  end
+
   def show
     @track = Track.find(params[:id])
+  end
+
+  def create
+    @track = Track.new(params[:track])
+    @track.save!
+
+    Mix.create! :song_id => @track.song_id, :track_id => @track.id
+
+    @track = Track.new :song_id => @track.song_id
+
+  rescue ActiveRecord::ActiveRecordError
+  ensure
+    render :action => 'show'
   end
 
   def update
     @track = Track.find(params[:id])
     @track.update_attributes! params[:track]
+    render(:update) { |page| page.hide 'editing' }
+
+  rescue ActiveRecord::ActiveRecordError
     render :action => 'show'
+  end
+
+  def destroy
+    Track.find(params[:id]).destroy
+    render(:update) { |page| page.hide 'editing' }
   end
 end
