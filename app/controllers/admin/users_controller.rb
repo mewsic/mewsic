6d7b7f2968d0cb1f456f65f68c57a3569af88100
Marcello Barnaba <vjt@openssl.it>
2008-07-07 14:11:53 +0000

    myousica: admin: added user management
    git-svn-id: file:///srv/svn/myousica/myousica/trunk@828 1758279c-6b0d-4dd2-8212-40bfc25400ca

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,15 @@
 class Admin::UsersController < Admin::AdminController
+  def index
+    @users = User.find(:all, :order => 'id')
+  end
+  
+  def show
+    @user = User.find_from_param(params[:id])
+  end
+
+  def update
+    @user = User.find_from_param(params[:id])
+    @user.update_attributes! params[:user]
+    render :action => 'show'
+  end
 end
