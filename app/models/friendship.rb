65c342393f0624b7d63a0ebb4398e22903cf5fcd
Marcello Barnaba <vjt@openssl.it>
2008-07-14 17:39:53 +0000

    myousica: added some validations to the friendship model
    git-svn-id: file:///srv/svn/myousica/myousica/trunk@946 1758279c-6b0d-4dd2-8212-40bfc25400ca

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -15,6 +15,9 @@ class Friendship < ActiveRecord::Base
   belongs_to :friendshipped_by_me,   :foreign_key => "user_id",   :class_name => "User"
   belongs_to :friendshipped_for_me,  :foreign_key => "friend_id", :class_name => "User"
 
+  validates_uniqueness_of :friend_id, :scope => :user_id
+  validates_uniqueness_of :user_id, :scope => :friend_id
+
   # TODO: Add some friendly accessor methods here
 
   after_save :fix_friends_count
