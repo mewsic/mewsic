bae29e6542f7350fe14a5bbde67f8cbd7c0a91b1
Marcello Barnaba <vjt@openssl.it>
2008-07-20 16:57:49 +0000

    myousica: cleaned up user helpers
    git-svn-id: file:///srv/svn/myousica/myousica/trunk@1025 1758279c-6b0d-4dd2-8212-40bfc25400ca

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -30,59 +30,41 @@ module UsersHelper
       %|<div id="change-gender">| << %w(male female other).map { |gender|
         image_tag gender_icon_path(gender), :alt => gender, :title => gender }.join << '</div>'
   end
-
-  def track_instrument_icon(track, options = {})
-    instrument_icon track.instrument, options
-  end     
-
-  def instrument_icon(instrument, options = {})
-    icon = instrument.icon.dup
-    icon.sub! /^instruments/, '\&/grey' if options[:grey]
-    image_tag icon, {:class => 'instrument', :alt => instrument.description, :rel => instrument.description, :size => '29x29'}.merge(options)
-  end
   
   def user_photo_link
-    content = ''
     unless @user.photos_url.blank?
-      content << %|<a class="tip-link" rel="tip-photos" title="PHOTOS" href="#"><img class="float-left" alt="" src="/images/icone_link_photo.gif"/>Photos</a><div id="tip-photos" style="display:none"><a href="#{@user.photos_url}" target="_new">#{@user.nickname}'s photos</a></div>|
+      %|<a class="tip-link" rel="tip-photos" title="PHOTOS" href="#"><img class="float-left" alt="" src="/images/icone_link_photo.gif"/>Photos</a><div id="tip-photos" style="display:none"><a href="#{@user.photos_url}" target="_new">#{@user.nickname}'s photos</a></div>|
     end
-    return content
   end
   
   def user_blog_link
-    content = ''
     unless @user.blog_url.blank?
-      content << %|<a class="tip-link" rel="tip-blog" title="BLOG" href="#"><img class="float-left" alt="" src="/images/icone_link_blog.gif"/>Blog</a><div id="tip-blog" style="display:none"><a href="#{@user.blog_url}" target="_new">#{@user.nickname}'s blog</a></div>|
+      %|<a class="tip-link" rel="tip-blog" title="BLOG" href="#"><img class="float-left" alt="" src="/images/icone_link_blog.gif"/>Blog</a><div id="tip-blog" style="display:none"><a href="#{@user.blog_url}" target="_new">#{@user.nickname}'s blog</a></div>|
     end
-    content
   end
   
   def user_myspace_link
-    content = ''
     unless @user.myspace_url.blank?
-      content << %|<a class="tip-link" rel="tip-myspace" title="MYSPACE PAGE" href="#"><img class="float-left" alt="" src="/images/icone_link_myspace.gif"/>MySpace</a><div id="tip-myspace" style="display:none"><a href="#{@user.myspace_url}" target="_new">#{@user.nickname}'s myspace</a></div>|
+      %|<a class="tip-link" rel="tip-myspace" title="MYSPACE PAGE" href="#"><img class="float-left" alt="" src="/images/icone_link_myspace.gif"/>MySpace</a><div id="tip-myspace" style="display:none"><a href="#{@user.myspace_url}" target="_new">#{@user.nickname}'s myspace</a></div>|
     end
-    content
   end
   
   def user_skype_link
-    content = ''
     if !@user.skype.blank? && @user.skype_public?
-      content << %|<a class="tip-link" rel="tip-skype" title="SKYPE CONTACT" href="#"><img class="float-left" alt="" src="/images/icone_link_skype.gif"/>Skype</a><div id="tip-skype" style="display:none"><a href="callto://#{@user.skype}">#{@user.skype}</a></div>|
+      %|<a class="tip-link" rel="tip-skype" title="SKYPE CONTACT" href="#"><img class="float-left" alt="" src="/images/icone_link_skype.gif"/>Skype</a><div id="tip-skype" style="display:none"><a href="callto://#{@user.skype}">#{@user.skype}</a></div>|
     end
-    content    
   end
   
   def user_msn_link
-    content = ''
     if !@user.msn.blank? && @user.msn_public?
-      content << %|<a class="tip-link" rel="tip-msn" title="MSN CONTACT" href="#"><img class="float-left" alt="" src="/images/icone_link_MSN.gif"/>MSN</a><div id="tip-msn" style="display:none"><a href="#{@user.msn}" class="msn-link">#{@user.msn}</a></div>|
+      %|<a class="tip-link" rel="tip-msn" title="MSN CONTACT" href="#"><img class="float-left" alt="" src="/images/icone_link_MSN.gif"/>MSN</a><div id="tip-msn" style="display:none"><a href="#{@user.msn}" class="msn-link">#{@user.msn}</a></div>|
     end
-    content    
   end
 
   def user_full_name(user)
-    "%s %s" % [user.first_name, user.last_name]
+    if @user.name_public?
+      %|<div class="mypage-name grey-text">#{"%s %s" % [user.first_name, user.last_name]}</div>|
+    end
   end
 
   def user_edit_button(field)
@@ -130,16 +112,5 @@ module UsersHelper
     link_to image_tag('refresh.png', :class => 'refresh-block'), top_users_path(url_options), :class => 'trigger'
   end
 
-  def idea_icon_for(track)
-    image_tag(track.idea? ? 'ideas_yes.png' : 'ideas_no.png', :class => 'idea-icon')
-  end
-  def idea_link_for(track)
-    image = idea_icon_for(track)
-    if track.user == current_user
-      link_to_remote image, :url => toggle_idea_user_track_path(current_user, track), :method => :put, :html => {:id => "toggle_idea_#{track.id}"}
-    else
-      image
-    end
-  end
 end
 
