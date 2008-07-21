c1aad5ffbeeccb5a288bab6ff7e91573f9e342cb
Marcello Barnaba <vjt@openssl.it>
2008-07-21 17:50:16 +0000

    myousica: fixed abuseable mailer test
    git-svn-id: file:///srv/svn/myousica/myousica/trunk@1058 1758279c-6b0d-4dd2-8212-40bfc25400ca

--- test/unit/abuse_mailer_test.rb
+++ test/unit/abuse_mailer_test.rb
@@ -2,12 +2,20 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class AbuseMailerTest < ActionMailer::TestCase
   tests AbuseMailer
-  def test_notification
-    @expected.subject = 'AbuseMailer#notification'
-    @expected.body    = read_fixture('notification')
-    @expected.date    = Time.now
 
-    assert_equal @expected.encoded, AbuseMailer.create_notification(@expected.date).encoded
+  def setup
+    ActionMailer::Base.delivery_method = :test
+    ActionMailer::Base.perform_deliveries = true
+    ActionMailer::Base.deliveries = []
+  end
+
+  def test_notification
+    abuseable = answers(:quentin_asks_about_magic)
+    abuse = Abuse.new :abuseable_id => abuseable.id, :abuseable_type => abuseable.class.name
+    sender = users(:quentin)
+
+    AbuseMailer.deliver_notification(abuseable, abuse, sender)
+    assert_equal 1, ActionMailer::Base.deliveries.size
   end
 
 end
