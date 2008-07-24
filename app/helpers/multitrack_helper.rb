0c4e05bf2b1f33e1d8cc3ee859fcc56cad7a00e9
Marcello Barnaba <vjt@openssl.it>
2008-07-24 18:49:09 +0000

    git-svn-id: file:///srv/svn/myousica/myousica/trunk@1119 1758279c-6b0d-4dd2-8212-40bfc25400ca

--- app/helpers/multitrack_helper.rb
+++ app/helpers/multitrack_helper.rb
@@ -2,4 +2,8 @@ module MultitrackHelper
   def instruments_used_in(song)
     song.tracks.map { |t| t.instrument.description }.join(', ')
   end
+
+  def session_key
+    "_myousica_session_key=#{cookies['_myousica_session_key']}"
+  end
 end
