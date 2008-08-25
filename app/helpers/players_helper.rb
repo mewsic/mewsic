4c87ab857d3ff559a953f1b1a06141a5fcc8c777
Marcello Barnaba <vjt@openssl.it>
2008-08-25 01:38:48 +0000

    myousica: closes http://adelao.lighthouseapp.com/projects/10984-myousica-testing/tickets/229-song-page-play-button
    git-svn-id: file:///srv/svn/myousica/myousica/trunk@1238 1758279c-6b0d-4dd2-8212-40bfc25400ca

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,10 +1,8 @@
 module PlayersHelper
   
   def play_button(playable, options = {})    
-    options = {}
-    options[:color] = playable.is_a?(Track) ? :orange : :green
-    options.merge(options)
-    image_tag "button_play_#{options[:color]}.png", :alt => 'PLAY', :class => 'player',
+    options.reverse_update(:color => playable.is_a?(Track) ? :orange : :green)
+    image_tag "button_play_#{options[:color]}#{options[:size]}.png", :alt => 'PLAY', :class => 'player',
       :rel => send("formatted_#{playable.class.name.downcase}_player_path", playable, 'html')
   end
   
