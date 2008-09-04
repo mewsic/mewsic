251a6fd8ede59aa10b3017415ab8e2a844bc0cd9
Marcello Barnaba <vjt@openssl.it>
2008-09-04 15:30:51 +0000

    myousica: added flash splash to the main page!
    git-svn-id: file:///srv/svn/myousica/myousica/trunk@1247 1758279c-6b0d-4dd2-8212-40bfc25400ca

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,23 +1,10 @@
 class DashboardController < ApplicationController
   
-  before_filter :redirect_unless_xhr, :only => [:splash, :top]
-  before_filter :find_splash_songs, :except => :noop
+  before_filter :redirect_unless_xhr, :only => :top
   session :off, :only => :noop
   
   def index
-    #@bands = User.find :all, :limit => 3, :include => [:avatars, :songs], :conditions => ["(users.type = 'Band' OR users.type = 'Dj') AND users.activated_at IS NOT NULL AND pictures.id IS NOT NULL AND songs.published = ? AND songs.id IS NOT NULL", true], :order => SQL_RANDOM_FUNCTION
-    #@answers = Answer.find :all, :order => 'answers.created_at DESC', :limit => 2, :include => [{:user => :avatars}], :conditions => ["users.activated_at IS NOT NULL"]
-    #@songs = Song.find :all, :limit => 3, :include => [{:user => :avatars}], :conditions => ['songs.published = ?', true], :order => SQL_RANDOM_FUNCTION
-    #@ideas = Track.find :all, :limit => 2, :include => {:owner => :avatars}, :conditions => ['tracks.idea = ?', true], :order => SQL_RANDOM_FUNCTION
-
     @people = find_top_myousicians :limit => 6
-
-    @motto = Motto.find :random
-  end
-
-  def splash
-    @motto = Motto.find :random
-    render :partial => 'splash'
   end
 
   def top
@@ -37,17 +24,6 @@ private
 
   def redirect_unless_xhr
     redirect_to '/' and return unless request.xhr?
-  end
-
-  def find_splash_songs    
-    @random_siblings = Song.find_random_direct_siblings(1)
-    if @random_siblings.size > 0
-      @splash_song = @random_siblings[0].song
-      @splash_songs = @splash_song.direct_siblings(3).unshift(@splash_song)
-      @splash_tracks = @splash_song.tracks.find(:all, :limit => 4)
-    else
-      @splash_songs, @splash_tracks = [], []
-    end    
   end
   
   def find_top_myousicians(options = {})
