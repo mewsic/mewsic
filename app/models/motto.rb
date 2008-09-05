4dff4b06fa488c60cadbc1d90670cfb7733d82ac
Marcello Barnaba <vjt@openssl.it>
2008-09-05 00:28:11 +0000

    myousica: removed old Motto model (now implemented via the flash splash)
    git-svn-id: file:///srv/svn/myousica/myousica/trunk@1251 1758279c-6b0d-4dd2-8212-40bfc25400ca

--- app/models/motto.rb
+++
@@ -1,7 +0,0 @@
-class Motto
-  @@phrases = File.read(File.join(RAILS_ROOT, 'app', 'views', 'dashboard', 'motto.txt')).split("\n")
-  def self.find(*args)
-    @@phrases.sort_by{rand}.first
-  end
-end
-
