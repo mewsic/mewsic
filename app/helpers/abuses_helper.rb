9d0d13ec1655dcd9dda8bcdfc167e30497b02e4e
Marcello Barnaba <vjt@openssl.it>
2008-09-12 18:25:03 +0000

    myousica: taller abuse lightview
    git-svn-id: file:///srv/svn/myousica/myousica/trunk@1305 1758279c-6b0d-4dd2-8212-40bfc25400ca

--- app/helpers/abuses_helper.rb
+++ app/helpers/abuses_helper.rb
@@ -1,5 +1,5 @@
 module AbusesHelper
   def report_abuse_link_to(url)
-    link_to('report abuse', url, :class => 'lightview', :rel => 'ajax', :title => 'Report abuse :: :: ajax:{method:"get"},width:480,height:330') if logged_in?
+    link_to('report abuse', url, :class => 'lightview', :rel => 'ajax', :title => 'Report abuse :: :: ajax:{method:"get"},width:480,height:350') if logged_in?
   end
 end
