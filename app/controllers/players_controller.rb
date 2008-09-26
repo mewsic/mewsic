865c2201811c7628dbc489644cce8d99599e6544
Marcello Barnaba <vjt@openssl.it>
2008-09-26 16:29:42 +0200

    Implemented a scrap of the safe playcount feature discussed in http://adelao.lighthouseapp.com/projects/10984/tickets/250-limite-play

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,14 +1,25 @@
 class PlayersController < ApplicationController
   
-  layout false
-  
   before_filter :find_playable
+  layout false
 
   def show
     redirect_to '/' and return unless @playable && request.xhr?
 
-    @playable.increment_listened_times    
-    @waveform = send("formatted_#{@playable.class.name.underscore}_path", @playable, 'png')
+    session[:playcount_barrier] = Time.now.to_i + @playable.seconds/2
+    @playable_type = @playable.class.name.underscore
+    @increment_path = send("#{@playable_type}_increment_path", @playable)
+    @waveform = send("formatted_#{@playable_type}_path", @playable, 'png')
+  end
+
+  def increment
+    if session[:playcount_barrier].nil? || Time.now.to_i < session[:playcount_barrier]
+      head :bad_request
+    else
+      @playable.increment_listened_times
+      head :ok
+    end
+    session[:playcount_barrier] = nil
   end
   
 private
