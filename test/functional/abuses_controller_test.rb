8d6fed72a5227b1fa117b81641dd9e0ee10409d1
Marcello Barnaba <vjt@openssl.it>
2008-09-28 15:15:41 +0200

    Added functional tests for abuses on users and tracks

--- test/functional/abuses_controller_test.rb
+++ test/functional/abuses_controller_test.rb
@@ -4,7 +4,7 @@ class AbusesControllerTest < ActionController::TestCase
 
   include AuthenticatedTestHelper
   
-  fixtures :users, :answers, :songs
+  fixtures :users, :answers, :songs, :tracks
     
   def test_should_new_redirect_if_request_is_get
     login_as :quentin
@@ -19,6 +19,14 @@ class AbusesControllerTest < ActionController::TestCase
 
     login_as :quentin
     get :new, :answer_id => 0
+    assert_response :bad_request
+
+    login_as :quentin
+    get :new, :track_id => 0
+    assert_response :bad_request
+
+    login_as :quentin
+    get :new, :user_id => 0
     assert_response :bad_request
   end
 
@@ -35,6 +43,54 @@ class AbusesControllerTest < ActionController::TestCase
       xhr :post, :create, :answer_id => answers(:quentin_asks_about_magic).id
       assert_response :success
       assert_equal Answer, assigns(:abuse).abuseable.class
+    end
+  end
+    
+  def test_should_show_get_and_assign_abuse_to_user
+    login_as :quentin
+    xhr :get, :new, :user_id => users(:john).id
+    assert_response :success
+    assert_equal User, assigns(:abuseable).class
+  end
+      
+  def test_should_create_and_assign_abuse_to_user
+    login_as :quentin
+    assert_difference 'Abuse.count' do
+      xhr :post, :create, :user_id => users(:john).id
+      assert_response :success
+      assert_equal User, assigns(:abuse).abuseable.class
+    end
+  end
+    
+  def test_should_show_get_and_assign_abuse_to_song
+    login_as :quentin
+    xhr :get, :new, :song_id => songs(:let_it_be).id
+    assert_response :success
+    assert_equal Song, assigns(:abuseable).class
+  end
+      
+  def test_should_create_and_assign_abuse_to_song
+    login_as :quentin
+    assert_difference 'Abuse.count' do
+      xhr :post, :create, :song_id => songs(:let_it_be).id
+      assert_response :success
+      assert_equal Song, assigns(:abuse).abuseable.class
+    end
+  end
+    
+  def test_should_show_get_and_assign_abuse_to_track
+    login_as :quentin
+    xhr :get, :new, :track_id => tracks(:sax_for_let_it_be).id
+    assert_response :success
+    assert_equal Track, assigns(:abuseable).class
+  end
+      
+  def test_should_create_and_assign_abuse_to_track
+    login_as :quentin
+    assert_difference 'Abuse.count' do
+      xhr :post, :create, :track_id => tracks(:sax_for_let_it_be).id
+      assert_response :success
+      assert_equal Track, assigns(:abuse).abuseable.class
     end
   end
   
@@ -55,22 +111,6 @@ class AbusesControllerTest < ActionController::TestCase
     assert assigns(:exists)
     assert_equal 1, ActionMailer::Base.deliveries.size
     assert_template 'new'
-  end
-    
-  def test_should_show_get_and_assign_abuse_to_song
-    login_as :quentin
-    xhr :get, :new, :song_id => songs(:let_it_be).id
-    assert_response :success
-    assert_equal Song, assigns(:abuseable).class
-  end
-      
-  def test_should_create_and_assign_abuse_to_song
-    login_as :quentin
-    assert_difference 'Abuse.count' do
-      xhr :post, :create, :song_id => songs(:let_it_be).id
-      assert_response :success
-      assert_equal Song, assigns(:abuse).abuseable.class
-    end
   end
   
 end
