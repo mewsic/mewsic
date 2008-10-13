04daba9decb4f44c1e77a1ab4e5f4c06a23fc638
Marcello Barnaba <vjt@openssl.it>
2008-10-13 12:00:26 +0200

    Documented the players and replies controller [#273]

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,10 +1,29 @@
+# Myousica Players Controller
+#
+# Copyright:: (C) 2008 Medlar s.r.l.
+# Copyright:: (C) 2008 Mikamai s.r.l.
+# Copyright:: (C) 2008 Adelao Group
+#
+# == Description
+# 
+# This simple controller handles the rendering of the Myousica Audio Player. Its only
+# action is +show+, requested via XHR. The stream to play is found via the +find_playable+
+# before filter.
+#
 class PlayersController < ApplicationController
   
   layout false
-  
+
   before_filter :redirect_to_root_unless_xhr
   before_filter :find_playable
 
+  # <tt>XHR GET /songs/:song_id/player.html</tt>
+  # <tt>XHR GET /tracks/:track_id/player.html</tt>
+  #
+  # Renders the <tt>app/views/show.html.erb</tt> view, which contains the JavaScript that
+  # initializes the player SWF object. Currently the play count is incremented when the 
+  # player is loaded, leading to easy abuse. It will be fixed soon.
+  #
   def show
     @playable.increment_listened_times    
     @waveform = send("formatted_#{@playable.class.name.underscore}_path", @playable, 'png')
@@ -12,6 +31,9 @@ class PlayersController < ApplicationController
   
 private
   
+  # Filter that founds a playable stream using the passed <tt>song_id</tt> or <tt>track_id</tt>
+  # parameter. If no playable is found, the user is redirected to '/'.
+  #
   def find_playable
     @playable = if params[:track_id]
       Track.find(params[:track_id])
--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,10 +1,29 @@
+# Myousica Players Controller
+#
+# Copyright:: (C) 2008 Medlar s.r.l.
+# Copyright:: (C) 2008 Mikamai s.r.l.
+# Copyright:: (C) 2008 Adelao Group
+#
+# == Description
+#
+# This simple controller implements the creation and rating of a +Reply+ object associated
+# to an Answer. Login is required to access both the +create+ and +rate+ actions.
+#
+# There is no <tt>new</tt> action because the reply creation form is embedded on answers
+# pages.
+#
 class RepliesController < ApplicationController
-  
+
   before_filter :login_required
-  
+
+  # <tt>POST /answers/:answer_id/replies</tt>
+  #
+  # Creates a new reply linked to the given <tt>answer_id</tt>, unless the answer is
+  # already closed (see Answer#closed?) or the model validation fails.
+  #
   def create
     @answer = Answer.find(params[:answer_id])
-    
+
     if @answer.closed?
       flash[:error] = 'The answer is closed'
     else
@@ -13,16 +32,20 @@ class RepliesController < ApplicationController
       @reply.answer = @answer
 
       if @reply.save
-        flash[:notice] = 'Reply has been saved correctly'      
+        flash[:notice] = 'Reply has been saved correctly'
       else
         flash[:error] = 'The body field is required!'
       end
     end
-    
+
     redirect_to answer_url(@answer)
   end
-  
-  def rate    
+
+  # <tt>PUT /replies/:id/rate</tt>
+  #
+  # Rates a reply, if it is rateable by the current user (see Reply#rateable_by?).
+  #
+  def rate
     @reply = Reply.find(params[:id])
     if @reply.rateable_by?(current_user)
       @reply.rate(params[:rate].to_i, current_user)
@@ -31,5 +54,5 @@ class RepliesController < ApplicationController
       render :nothing => true, :status => :bad_request
     end
   end
-  
+
 end
