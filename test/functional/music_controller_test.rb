bbaacd64615abf9dbe9001b3ff1cc9bb27928995
Marcello Barnaba <vjt@openssl.it>
2008-10-16 03:54:22 +0200

    stronger music controller functional tests

--- test/functional/music_controller_test.rb
+++ test/functional/music_controller_test.rb
@@ -1,25 +1,25 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class MusicControllerTest < ActionController::TestCase
-  tests MusicController
+  fixtures :genres, :songs, :tracks, :mixes
 
   # Replace this with your real tests.
   def test_index
     get :index
+    assert_response :success
+
+    assert assigns(:best_songs)
+    assert_equal 3, assigns(:best_songs).size
     
-    # Il numero di canzoni x ogni genere è random e quindi non testabile
-    #
-    # assert assigns(:best_songs)
-    #     assert_equal 3, assigns(:best_songs).size
-    #     
-    #     assert assigns(:most_used_tracks)
-    #     assert_equal 3, assigns(:most_used_tracks).size
-    #     
-    #     assert assigns(:genres)
-    #     assert_equal 5, assigns(:genres).size
-    #     
-    #     assert assigns(:newest_songs)
-    #     assert_equal 3, assigns(:newest_songs).size
+    assert assigns(:most_used_tracks)
+    assert_equal 3, assigns(:most_used_tracks).size
+
+    assert assigns(:newest)
+    assert_equal 5, assigns(:newest).size
         
+    assert assigns(:genre_chars)
+    assert assigns(:genres)
+    assert assigns(:genres).all? { |g| g.name.first.upcase == assigns(:genre_chars).first }
+    assert assigns(:genres).all? { |g| g.published_songs.count > 0 }
   end
 end
