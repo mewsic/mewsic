78bface15978f3a04800146e801388f490947c0f
Marcello Barnaba <vjt@openssl.it>
2008-10-16 18:44:27 +0200

    Documented the InstrumentCategory model [#273]

--- app/models/instrument_category.rb
+++ app/models/instrument_category.rb
@@ -7,7 +7,19 @@
 #  created_at  :datetime      
 #  updated_at  :datetime      
 #
-
+# == Description
+#
+# This super-simple model represents an instrument category.
+#
+# == Associations
+#
+# * <b>has_many</b> <tt>instruments</tt>, linked via the Instrument <tt>category_id</tt>
+#   attribute.
+#
+# == Validations
+#
+# * <b>validates_presence_of</b> <tt>description</tt>
+#
 class InstrumentCategory < ActiveRecord::Base
   has_many :instruments, :foreign_key => 'category_id'
   validates_presence_of :description
