e5e8feee894164d5bd05d6c2ed418f99ab92d9ab
Aleksandr Kreynin <aleksandr.kreynin@adelao.it>
2008-10-22 00:38:00 +0200

    Added bands_and_deejays functional test [#282]

--- test/functional/bands_and_deejays_controller_test.rb
+++ test/functional/bands_and_deejays_controller_test.rb
@@ -1,8 +1,18 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class BandsAndDeejaysControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  fixtures :users, :mbands
+   
+  def test_index
+    get :index
+    assert_response :success
+    
+    assert assigns(:coolest)
+    assert assigns(:best_myousicians)
+    assert assigns(:prolific)
+    assert assigns(:most_admired)
+    assert assigns(:coolest_mbands)
+    assert assigns(:newest)
+    assert assigns(:most_instruments)
   end
 end
