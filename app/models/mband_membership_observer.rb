f1ecf2e836dfb4317b991b3ec6fbd7b6a3287fdc
Marcello Barnaba <vjt@openssl.it>
2008-10-22 19:30:41 +0200

    fixed wrong decline link generation in mband invitation messages

--- app/models/mband_membership_observer.rb
+++ app/models/mband_membership_observer.rb
@@ -6,7 +6,7 @@ class MbandMembershipObserver < ActiveRecord::Observer
     body    = <<-EOM
 <p>Click <a href="#{APPLICATION[:url]}/mbands/#{membership.mband.to_param}">here</a> to view the mband profile.</p>
 <p>Click <a href="#{APPLICATION[:url]}/mband_memberships/accept/#{membership.membership_token}">here</a> to accept the invitation.</p>
-<p>Click <a href="#{APPLICATION[:url]}/mband_memberships/decline/#{membership.id}">here</a> to decline the invitation.</p>
+<p>Click <a href="#{APPLICATION[:url]}/mband_memberships/decline/#{membership.membership_token}">here</a> to decline the invitation.</p>
     EOM
 
     @message = Message.new(:subject => subject, :body => body)
