ba6e51d25b21c9c368d3416fa2b162901a0db34a
Aleksandr Kreynin <aleksandr.kreynin@adelao.it>
2008-10-23 06:02:13 +0200

    Updated messages controller test

--- test/functional/messages_controller_test.rb
+++ test/functional/messages_controller_test.rb
@@ -16,26 +16,72 @@ class MessagesControllerTest < ActionController::TestCase
     ActionMailer::Base.deliveries = []
   end
 
-  def test_index_redirect_unless_logged
+  def test_redirect_to_index_if_not_xhr
     get :index, :user_id => users(:quentin)
     assert_response :redirect
   end
-  
-  def test_index_redirect_if_logged_in_but_not_current_user
-    login_as :user_10
-    get :index, :user_id => users(:quentin)
+
+  def test_index_redirect_unless_logged
+    xhr :get, :index, :user_id => users(:quentin)
     assert_response :redirect
+  end
+  
+  def test_index_redirect_if_user_not_found
+    xhr :get, :index, :user_id => 'pippppppooo'
+    assert_response :redirect
+  end
+  
+  def test_index_forbidden_if_logged_in_but_not_mailbox_owner
+    login_as :user_10
+    xhr :get, :index, :user_id => users(:quentin)
+    assert_response :forbidden
   end
   
   def test_index_should_show_messages
     login_as :quentin
     xhr :get, :index, :user_id => users(:quentin)
     assert_response :success
+    assert_template 'index'
+  end
+  
+  def test_sent_should_show_messages
+    login_as :quentin
+    xhr :get, :sent, :user_id => users(:quentin)
+    assert_response :success
+    assert_template 'index'
+  end
+  
+  def test_unread_should_show_messages
+    login_as :quentin
+    xhr :get, :unread, :user_id => users(:quentin)
+    assert_response :success
+    assert_template 'index'
+  end
+
+  def test_new_should_show_form
+    login_as :quentin
+    xhr :get, :new, :user_id => users(:quentin)
+    assert_response :success
+    assert_template 'new'
+  end
+  
+  def test_new_should_show_form_if_reply
+    login_as :quentin
+    xhr :get, :new, :user_id => users(:quentin), :reply => messages(:message_for_quentin_1)
+    assert_response :success
+    assert_template 'new'
+  end
+  
+  def test_new_should_show_form_if_params_to
+    login_as :quentin
+    xhr :get, :new, :user_id => users(:quentin), :to => users(:user_10)
+    assert_response :success
+    assert_template 'new'
   end
   
   def test_index_should_redirect_if_page_bigger_then_existing_pages
     login_as :quentin
-    get :index, :user_id => users(:quentin), :page => 1000
+    xhr :get, :index, :user_id => users(:quentin), :page => 1000
     assert_response :redirect
   end
   
@@ -61,12 +107,27 @@ class MessagesControllerTest < ActionController::TestCase
     user_10_received_count = users(:user_10).received_messages.count
     assert_difference 'Message.count' do
       xhr :post, :create, :user_id => users(:quentin), :message => { :subject => 'Hello', :body => 'hello world!', :to => users(:user_10).login }
+      assert_response :success
     end
     assert_equal quentin_sent_count + 1,      users(:quentin).sent_messages.count
     assert_equal user_10_received_count + 1,  users(:user_10).received_messages.count
     assert_equal 1, ActionMailer::Base.deliveries.size
   end
-  
+ 
+  def test_should_destroy_message
+    assert_difference 'Message.count', -1 do
+      login_as :quentin
+      xhr :delete, :destroy, :user_id => users(:quentin), :id => messages(:message_for_quentin_1) 
+      assert_response :success
+      assert true, messages(:message_for_quentin_1).recipient_deleted
+      logout 
+      login_as :user_10
+      xhr :delete, :destroy, :user_id => users(:user_10), :id => messages(:message_for_quentin_1) 
+      assert_response :success
+      assert true, messages(:message_for_quentin_1).sender_deleted
+    end
+  end
+ 
   def test_should_create_message_for_multiple_recipients
     login_as :quentin
     quentin_sent_count = users(:quentin).sent_messages.count
@@ -74,6 +135,7 @@ class MessagesControllerTest < ActionController::TestCase
     user_11_received_count = users(:user_11).received_messages.count
     assert_difference 'Message.count', 2 do
       xhr :post, :create, :user_id => users(:quentin), :message => { :subject => 'Hello', :body => 'hello world!', :to => "#{users(:user_10).login}, #{users(:user_11).login}" }
+      assert_response :success
     end
     assert_equal quentin_sent_count + 2,      users(:quentin).sent_messages.count
     assert_equal user_10_received_count + 1,  users(:user_10).received_messages.count
@@ -83,7 +145,8 @@ class MessagesControllerTest < ActionController::TestCase
   def test_should_not_create_message
     login_as :quentin
     assert_no_difference 'Message.count' do
-      post :create, :user_id => users(:quentin), :message => { :subject => 'Hello', :body => 'hello world!', :to => 'pipppppo' }
+      xhr :post, :create, :user_id => users(:quentin), :message => { :subject => 'Hello', :body => 'hello world!', :to => 'pipppppo' }
+      assert_response :success
     end
   end
   
