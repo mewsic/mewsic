44a466aa55c244dc1a43db49e3de037e77f4c51c
Marcello Barnaba <vjt@openssl.it>
2008-11-14 16:51:36 +0100

    Hack: allow the User admin controller to update user's email

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -10,6 +10,7 @@ class Admin::UsersController < Admin::AdminController #:nodoc:
   def update
     @user = User.find_from_param(params[:id])
     if @user.update_attributes! params[:user]
+      @user.update_attribute :email, params[:user][:email]
       render(:update) { |page| page.hide 'editing' }
     else
       render :action => 'show'
