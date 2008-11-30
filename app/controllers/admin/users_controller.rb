a7178b223096e6c5d120d4606379bdd697cf25e0
Marcello Barnaba <vjt@openssl.it>
2008-11-30 15:42:44 +0100

    Added user activation/deactivation via admin interface [#289 state:committed]

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,19 +1,26 @@
 class Admin::UsersController < Admin::AdminController #:nodoc:
+  before_filter :find_user, :except => :index
+
   def index
     @users = User.find(:all, :order => 'id DESC')
   end
   
   def show
-    @user = User.find_from_param(params[:id])
   end
 
   def update
-    @user = User.find_from_param(params[:id])
     if @user.update_attributes! params[:user]
       @user.update_attribute :email, params[:user][:email]
+      @user.update_attribute :activated_at, params[:user][:activated_at]
+
       render(:update) { |page| page.hide 'editing' }
     else
       render :action => 'show'
     end
+  end
+
+  protected
+  def find_user
+    @user = User.find_by_login(params[:id]) || User.find(params[:id])
   end
 end
