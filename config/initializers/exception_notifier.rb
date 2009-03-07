b4a43f474f0e276a95fbd9bb4e40d45e2ebb3c4a
Marcello Barnaba <vjt@openssl.it>
2009-03-07 06:18:19 +0100

    Fixed exception notifier from: address

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,3 +1,3 @@
 ExceptionNotifier.exception_recipients = %w(aleksandr.kreynin@gmail.com marcello.barnaba@gmail.com)
-ExceptionNotifier.sender_address = %("Mewsic Application Error" <error@mewsic.stage.lime5.it>)
+ExceptionNotifier.sender_address = %("Mewsic Application Error" <robot@lime5.it>)
 ExceptionNotifier.email_prefix = "[MEWSIC ERROR] "
