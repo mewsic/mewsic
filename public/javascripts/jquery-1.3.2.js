0cca081a0b3799556859bf2c3053efd641bb32ad
Marcello Barnaba <vjt@openssl.it>
2009-03-11 21:26:41 +0100

    Added images, javascripts and stylesheets [#2]

---
+++ public/javascripts/jquery-1.3.2.js
@@ -0,0 +1,333 @@
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+ <script type="text/javascript">
+ 
+ (function(){
+function a(){this.t={};this.tick=function(b){this.t[b]=(new Date).getTime()};this.tick("start")}var c=new a;window.jstiming={Timer:a,load:c};try{window.jstiming.pt=window.external.pageT}catch(d){};
+})();
+
+ 
+ 
+ 
+ var codesite_token = null;
+ 
+ 
+ </script>
+ <title>jquery-1.3.2.js - 
+ jqueryjs -
+ 
+ jQuery 1.3.2 - Google Code</title>
+ <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" >
+ 
+ <link type="text/css" rel="stylesheet" href="http://www.gstatic.com/codesite/ph/969254271741402528/css/d_20081117.css">
+ 
+ 
+ 
+<!--[if IE]>
+ <link type="text/css" rel="stylesheet" href="http://www.gstatic.com/codesite/ph/969254271741402528/css/d_ie.css" >
+<![endif]-->
+</head>
+<body class="t2">
+ <div id="gaia">
+ 
+ <span>
+ 
+ <a href="/p/support/wiki/WhatsNew" style="color:#a03">What's new?</a>
+ | <a href="/p/support/">Help</a>
+ | <a href="/more/">Directory</a>
+ | <a href="http://www.google.com/accounts/Login?continue=http%3A%2F%2Fcode.google.com%2Fp%2Fjqueryjs%2Fdownloads%2Fdetail%3Fname%3Djquery-1.3.2.js&amp;followup=http%3A%2F%2Fcode.google.com%2Fp%2Fjqueryjs%2Fdownloads%2Fdetail%3Fname%3Djquery-1.3.2.js">Sign in</a>
+ 
+ </span>
+
+ </div>
+ <div class="gbh" style="left: 0pt;"></div>
+ <div class="gbh" style="right: 0pt;"></div>
+ 
+ 
+ <div style="height: 1px"></div>
+ <table style="padding:0px; margin: 20px 0px 0px 0px; width:100%" cellpadding="0" cellspacing="0">
+ <tr>
+ <td style="width:153px; text-align:center;">
+ <a href="/">
+ 
+ <img src="http://www.gstatic.com/codesite/ph/images/code_sm.png"
+ width="153" height="55" alt="Google">
+ 
+ </a>
+ </td>
+ <td style="padding-left: 0.8em">
+ 
+ <div id="pname" style="margin: 0px 0px -3px 0px">
+ <a href="/p/jqueryjs/" style="text-decoration:none; color:#000">jqueryjs</a>
+ </div>
+ <div id="psum">
+ <i><a id="project_summary_link" href="/p/jqueryjs/" style="text-decoration:none; color:#000">jQuery JavaScript Library</a></i>
+ </div>
+ 
+ </td>
+ <td style="white-space:nowrap; text-align:right">
+ 
+ <form action="/hosting/search">
+ <input size="30" name="q" value="">
+ <input type="submit" name="projectsearch" value="Search Projects" >
+ </form>
+ 
+ </tr>
+ </table>
+
+
+<table id="mt" cellspacing="0" cellpadding="0" width="100%" border="0">
+ <tr>
+ <th onclick="if (!cancelBubble) _go('/p/jqueryjs/');">
+ <div class="tab inactive">
+ <div class="round4"></div>
+ <div class="round2"></div>
+ <div class="round1"></div>
+ <div class="box-inner">
+ <a onclick="cancelBubble=true;" href="/p/jqueryjs/">Project&nbsp;Home</a>
+ </div>
+ </div>
+ </th><td>&nbsp;&nbsp;</td>
+ 
+ 
+ 
+ 
+ <th onclick="if (!cancelBubble) _go('/p/jqueryjs/downloads/list');">
+ <div class="tab active">
+ <div class="round4"></div>
+ <div class="round2"></div>
+ <div class="round1"></div>
+ <div class="box-inner">
+ <a onclick="cancelBubble=true;" href="/p/jqueryjs/downloads/list">Downloads</a>
+ </div>
+ </div>
+ </th><td>&nbsp;&nbsp;</td>
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ <th onclick="if (!cancelBubble) _go('/p/jqueryjs/source/checkout');">
+ <div class="tab inactive">
+ <div class="round4"></div>
+ <div class="round2"></div>
+ <div class="round1"></div>
+ <div class="box-inner">
+ <a onclick="cancelBubble=true;" href="/p/jqueryjs/source/checkout">Source</a>
+ </div>
+ </div>
+ </th><td>&nbsp;&nbsp;</td>
+ 
+ 
+ <td width="100%">&nbsp;</td>
+ </tr>
+</table>
+<table cellspacing="0" cellpadding="0" width="100%" align="center" border="0" class="st">
+ <tr>
+ 
+ 
+ 
+ 
+ <td>
+ <div class="issueDetail">
+<div class="isf">
+ 
+ <span class="inIssueList"> 
+ <span>Search</span>
+ <form action="list" method="GET" style="display:inline">
+ <select id="can" name="can" style="font-size:92%">
+ <option disabled="disabled">Search Within:</option>
+ 
+ <option value="1" >&nbsp;All Downloads</option>
+ <option value="3" >&nbsp;Featured Downloads</option>
+ <option value="2" selected="selected">&nbsp;Current Downloads</option>
+ 
+ 
+ <option value="4" >&nbsp;Deprecated Downloads</option>
+ 
+ </select>
+ <span>for</span>
+ <input type="text" size="32" id="q" name="q" value="" style="font-size:92%" >
+ 
+ 
+ <input type="submit" value="Search" style="font-size:92%" >
+ </form>
+ </span>
+
+ 
+ 
+ 
+
+</div>
+</div>
+
+ </td>
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ <td height="4" align="right" valign="top" class="bevel-right">
+ <div class="round4"></div>
+ <div class="round2"></div>
+ <div class="round1"></div>
+ </td>
+ </tr>
+</table>
+<script type="text/javascript">
+ var cancelBubble = false;
+ function _go(url) { document.location = url; }
+</script>
+
+<div id="maincol">
+<!-- IE -->
+
+
+
+<div id="issueheader">
+<table cellpadding="0" cellspacing="0" width="100%"><tbody>
+ <tr>
+ <td class="vt h3" nowrap="nowrap"> &nbsp;Download:
+ 
+ </td>
+ <td width="90%" class="vt">
+ 
+ <span class="h3" >jQuery 1.3.2</span>
+ </td>
+ </tr>
+ <tr>
+ <td></td>
+ <td nowrap="nowrap">
+ 
+ &nbsp;
+ 
+ </td>
+ </tr>
+</tbody></table>
+</div><table width="100%" cellpadding="0" cellspacing="0" border="0" class="issuepage">
+<tbody class="opened expand">
+ <tr>
+ <td id="issuemeta" rowspan="1000"> 
+ <table cellspacing="0" cellpadding="0">
+ <tr><th align="left">Uploaded&nbsp;by:&nbsp;</th><td width="100%">
+ <a href="/u/jeresig/">jeresig</a>
+ </td></tr>
+ <tr><th align="left">Uploaded:&nbsp;</th><td>
+ <span class="date" style="white-space:nowrap" title="Thu Feb 19 15:02:00 2009">Feb 19, 2009</span>
+ </td></tr>
+ <tr><th align="left">Downloads:&nbsp;</th><td>154651</td></tr>
+ 
+ 
+ <tr><td colspan="2">
+ <a href="list?q=label:Type-Source"
+ title=""
+ class="label"><b>Type-</b>Source</a>
+ </td></tr>
+ 
+ 
+ 
+ <tr><td colspan="2"><a href="list?q=label:Featured"
+ title=""
+ class="label">Featured</a>
+ </td></tr>
+ 
+ 
+ </table>
+<br>
+<br>
+<br>
+ </td>
+ <td class="vt issuedescription" width="100%" style="padding:16px">
+ <table><tr><td>
+ <div style="background:#ddf8cc; margin-bottom: 5px">
+ <div class="round4"></div>
+ <div class="round2"></div>
+ <div class="round1"></div>
+ <div class="box-inner">
+ <a 
+ href="http://jqueryjs.googlecode.com/files/jquery-1.3.2.js"><img src="http://www.gstatic.com/codesite/ph/images/dl_arrow.gif" ></a>&nbsp;
+ <a style="font-size: 140%"
+ 
+ href="http://jqueryjs.googlecode.com/files/jquery-1.3.2.js">jquery-1.3.2.js</a> &nbsp; 117 KB
+ </div>
+ <div class="round1"></div>
+ <div class="round2"></div>
+ <div class="round4"></div>
+ </div>
+ </td></tr></table>
+ <span style="white-space:nowrap">SHA1 Checksum: f0b95e99225f314fbe37ccf6b74ce2f916c517de</span><br>
+ <b>Tip:</b> Use the SHA1 checksum shown to verify file integrity. 
+ </td>
+ </tr>
+</tbody>
+</table>
+<br>
+<br>
+<br>
+<script type="text/javascript" src="http://www.gstatic.com/codesite/ph/969254271741402528/js/dit_scripts_20081013.js"></script>
+
+
+ 
+
+ 
+ <script type="text/javascript" src="http://www.gstatic.com/codesite/ph/969254271741402528/js/core_scripts_20081103.js"></script>
+ 
+ 
+ 
+ 
+ 
+ </div>
+<div id="footer" dir="ltr">
+ 
+ <div class="text">
+ 
+ &copy;2008 Google -
+ <a href="/">Code Home</a> -
+ <a href="/tos.html">Terms of Service</a> -
+ <a href="http://www.google.com/privacy.html">Privacy Policy</a> -
+ <a href="/more/">Site Directory</a>
+ 
+ </div>
+</div>
+<script type="text/javascript">
+/**
+ * Reports analytics.
+ * It checks for the analytics functionality (window._gat) every 100ms
+ * until the analytics script is fully loaded in order to invoke siteTracker.
+ */
+function _CS_reportAnalytics() {
+ window.setTimeout(function() {
+ if (window._gat) {
+ var siteTracker = _gat._getTracker("UA-18071-1");
+ siteTracker._initData();
+ siteTracker._trackPageview();
+ var projectTracker = _gat._getTracker("UA-1076265-1");
+projectTracker._initData();
+projectTracker._trackPageview();
+ } else {
+ _CS_reportAnalytics();
+ }
+ }, 100);
+}
+</script>
+
+ 
+ 
+
+
+ 
+ 
+ <script type="text/javascript">_CS_reportToCsi();</script>
+ 
+ </body>
+</html>
+
