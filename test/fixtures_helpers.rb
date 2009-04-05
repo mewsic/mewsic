f98411e8242591331b778576c34937b5d35b5c96
Marcello Barnaba <vjt@openssl.it>
2009-04-05 20:49:16 +0200

    Refactored random_* fixture methods, and moved them into fixtures_helpers.rb.

--- test/fixtures_helpers.rb
+++ test/fixtures_helpers.rb
@@ -1,3 +1,57 @@
+# XXX FIXME this file should contain constants for every object class
+# present into fixtures, and be included in every of them.
+# e.g. SONG_COUNT=200 MBAND_SONG_COUNT=50 USER_COUNT=500 and so on.
+#
+def random_rating
+  rand * 5
+end
+
+def random_title
+  shuffle(lorem_ipsum[0..64]).capitalize + '.'
+end
+
+def random_description
+  shuffle(lorem_ipsum).capitalize
+end
+
+def random_genre
+  genres.sort_by{rand}.first
+end
+
+def random_user
+  "user_#{rand(500)}"
+end
+
+def random_avatar
+  @avatars ||= Dir['test/fixtures/files/avatars/*']
+  @avatars.rand
+end
+
+def picture(name)
+  "test/fixtures/files/#{name}"
+end
+
+# Randomly sorts the given array, truncating it to a
+# random length, and joining its elements with spaces.
+#
 def shuffle(array)
-  array.sort_by{rand}.join(' ')[0..rand(array.size)]
+  array.sort_by{rand}[0..rand(array.size)].join(' ')
+end
+
+# Returns lorem_ipsum as an array
+#
+def lorem_ipsum
+  @lipsum ||= File.read(txt'lorem_ipsum').split(' ')
+end
+
+# Returns an array with genres residing in genres.txt
+#
+def genres
+  @genres ||= File.readlines(txt'genres').map(&:strip)
+end
+
+# Read a txt file from test/fixtures/name.txt
+#
+def txt(name)
+  File.join(RAILS_ROOT, 'test', 'fixtures', name + '.txt')
 end
